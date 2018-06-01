27f49de13300249e2296e7e95f3cc31fff704f3b
Jim van Leeuwen <jim.vanleeuwen@student.hu.nl>
2018-06-01 15:40:36 +0200

    restart

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include "BinaryCode.h"
 
 int main() {
-    Binary binarySet;
-    std::cout << binarySet.NOOP.getBinaryCode() << std::endl;
-    std::cout << binarySet.STRA.getBinaryCode() << std::endl;
+
+
+
     return 0;
 }
